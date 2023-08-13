e410050948da3b8a8e1671bdcb54e6820a5fae10
ashmitpatel1402 <ashmit14patel@gmail.com>
2023-08-13 15:03:05 +0530

    Added error files for first time

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,16 @@
+class AppError extends Error{
+    constructor(
+        name,
+        message,
+        explanation,
+        statusCode
+    ){
+        super();
+        this.name=name;
+        this.explanation=explanation;
+        this.message=message;
+        this.statusCode=statusCode
+    }
+}
+
+module.exports=AppError;
