ebb5ff4deff31cc7f362afa89e9b129e4abe54dd
ashmitpatel1402 <ashmit14patel@gmail.com>
2023-08-13 17:40:42 +0530

    Added new colums to model using migration

---
+++ src/migrations/20230813115819-modify_bookings_add_new_fields.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Bookings',
+       'noOfSeats',
+       {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        default:1
+       }
+    );
+       await queryInterface.addColumn('Bookings',
+       'totalCost',
+       {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        default:0
+       }
+
+
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
